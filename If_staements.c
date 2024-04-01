69017b95e1f329be7d1a30ab0ad392ca8e41826c
Prithveeyy <prithvee2118@gmail.com>
2024-04-01 17:15:26 +0530

    asd

--- If_staements.c
+++ If_staements.c
@@ -5,10 +5,24 @@ int main()
 
 int age ;
 
-printf("Enter your age");
-scanf("%lf",age);
+printf("Enter your age \t");
+scanf("%d", &age);
 
-
+if (age >= 18)
+{
+    printf("you are eligible !");
+}
+else if (age < 0 )
+{
+    printf("You are not born yet :)");
+}
+else if (age == 0)
+{
+    printf("you have just been born today\n Happy birthday !");
+}
+else {
+ printf("you are not old enough fot this");
+}
 
 return 0;
 }
