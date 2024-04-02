b7f186a3c51996c3795ab5a0bcc61a719e48e798
Prithveeyy <prithvee2118@gmail.com>
2024-04-02 11:46:05 +0530

    project

--- projects/rating.c
+++ projects/rating.c
@@ -1,22 +1,76 @@
 #include <stdio.h>
 
-int addNumbers(int a, int b) {
-    int sum = a + b; // Add the numbers
-    return sum; // Return the result
+float calculateFIFARating(int goals, int assists, float passaccuracy, float tackleaccuracy, float dribblerate) {
+
+    float w1 = 40.0 / 100;   
+    float w2 = 30.0 / 100;   
+    float w3 = 15.0 / 100;   
+    float w4 = 10.0 / 100;   
+    float w5 = 5.0 / 100;    
+    
+    
+    float FIFARating = goals * w1 + assists * w2 + passaccuracy * w3 + tackleaccuracy * w4 + dribblerate * w5;
+
+    
+    if (FIFARating > 100.0)
+        FIFARating = 100.0;
+    
+    return FIFARating;
 }
+
 int main() {
-    int num1;
-    int num2;
+    int goals;
+    int assists;
+    int pass;
+    int passsuc;
+    int tackle;
+    int tacklesuc;
+    int drrible;
+    int dribblesuc;
 
-    printf("what is the number of goals scored in first season ? ");
-    scanf("%d", &num1);    
-    
-    printf("what is the number of goals scored in second season ? ");
-    scanf("%d", &num2);
+    printf("What is the number of goals scored? ");
+    scanf("%d", &goals);
 
-    int result = addNumbers(num1, num2);
+    printf("What is the number of assists provided? ");
+    scanf("%d", &assists);
+
+    printf("Enter total passes: ");
+    scanf("%d", &pass);
+
+    printf("Enter successful passes: ");
+    scanf("%d", &passsuc);
+    if (passsuc > pass) {
+        printf("Error: Successful passes cannot be more than total passes.\n");
+        return 1; // Exiting program with an error code
+    }
+    float passaccuracy = (float)passsuc / pass * 100;
+    printf("Pass accuracy: %.2f%%\n", passaccuracy);
+
+    printf("Enter total tackle attempts: ");
+    scanf("%d", &tackle);
+
+    printf("Enter successful tackle attempts: ");
+    scanf("%d", &tacklesuc);
+    if (tacklesuc > tackle) {
+        printf("Error: Successful tackles cannot be more than total tackles.\n");
+        return 1; // Exiting program with an error code
+    }
+    float tackleaccuracy = (float)tacklesuc / tackle * 100;
+    printf("Tackle accuracy: %.2f%%\n", tackleaccuracy);
     
-    printf("Total goals scored is: %d\n", result);
-    
+    printf("Enter total dribble attempts: ");
+    scanf("%d", &drrible);
+    printf("Enter successful dribble attempts: ");
+    scanf("%d", &dribblesuc);
+    if (dribblesuc > drrible) {
+        printf("Error: Successful dribbles cannot be more than total dribbles.\n");
+        return 1; // Exiting program with an error code
+    }
+    float dribblerate = (float)dribblesuc / drrible * 100;
+    printf("Dribble success rate: %.2f%%\n", dribblerate);
+
+    float rating = calculateFIFARating(goals, assists, passaccuracy, tackleaccuracy, dribblerate);
+    printf("\nFIFA Rating: %.2f\n\n", rating);
+
     return 0;
 }
