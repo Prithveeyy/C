4ec22baab368162bb010f0d386bf8cd6baeb78e2
Prithveeyy <prithvee2118@gmail.com>
2024-04-02 17:12:40 +0530

    s

---
+++ Assignment/assignment1.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(){
+
+int row;
+int column;
+
+ for (int i = 0 ; i <5; i++)
+    {
+        for (int j = 0; j <5 ; j++)
+        {
+            printf("*");
+        }
+        printf("\n");
+        
+    }  
+
+
+}
