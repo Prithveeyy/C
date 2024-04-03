e7310062724d70f45bdeda6fe1b1b6fb4fb37609
Prithveeyy <prithvee2118@gmail.com>
2024-04-03 10:51:46 +0530

    assignment

---
+++ Assignment/assignment_4/assignment_4.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int main() {
+    int num;
+    bool prime = true; 
+
+    printf("Enter number : ");
+    scanf("%d", &num);
+
+    for (int i = 2; i <= num / 2; i++) {
+        if (num % i == 0) {
+            prime = false; 
+            break; 
+        }
+    }
+
+    if (prime == true) { 
+        printf("The number is prime.\n");
+    } else {
+        printf("The number is not prime.\n");
+    }
+
+    return 0;
+}
