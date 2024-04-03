58b7ec206518528531e1cd4953264d761c95f828
Prithveeyy <prithvee2118@gmail.com>
2024-04-03 12:54:48 +0530

    C language Project

--- projects/marks/marks.c
+++ projects/marks/marks.c
@@ -23,42 +23,42 @@ int main() {
     scanf("%d", &science);
     if (science < 0 || science > 100) {
         printf("Invalid marks! Please enter marks between 0 and 100.\n");
-        return 1; // Exiting with error code
+        return 1; 
     }
 
     printf("Enter marks for Maths: ");
     scanf("%d", &maths);
     if (maths < 0 || maths > 100) {
         printf("Invalid marks! Please enter marks between 0 and 100.\n");
-        return 1; // Exiting with error code
+        return 1; 
     }
 
     printf("Enter marks for English: ");
     scanf("%d", &english);
     if (english < 0 || english > 100) {
         printf("Invalid marks! Please enter marks between 0 and 100.\n");
-        return 1; // Exiting with error code
+        return 1; 
     }
 
     printf("Enter marks for Social Science: ");
     scanf("%d", &socialScience);
     if (socialScience < 0 || socialScience > 100) {
         printf("Invalid marks! Please enter marks between 0 and 100.\n");
-        return 1; // Exiting with error code
+        return 1; 
     }
 
     printf("Enter marks for 2nd Language: ");
     scanf("%d", &secondLanguage);
     if (secondLanguage < 0 || secondLanguage > 100) {
         printf("Invalid marks! Please enter marks between 0 and 100.\n");
-        return 1; // Exiting with error code
+        return 1; 
     }
 
     printf("Enter marks for Elective: ");
     scanf("%d", &elective);
     if (elective < 0 || elective > 100) {
         printf("Invalid marks! Please enter marks between 0 and 100.\n");
-        return 1; // Exiting with error code
+        return 1; 
     }
 
     int totalMarks = science + maths + english + socialScience + secondLanguage + elective;
